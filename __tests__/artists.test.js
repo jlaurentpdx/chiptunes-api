4c4fad5a75b9d2abbdfbd7d73e4a40cbc5055341
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-03-18 18:57:26 -0700

    5/5 artists delete test - written

--- __tests__/artists.test.js
+++ __tests__/artists.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Artist = require('../lib/models/Artist');
 
 describe('artists routes', () => {
   beforeEach(() => {
@@ -69,6 +70,13 @@ describe('artists routes', () => {
     const resp = await request(app)
       .patch('/api/v1/artists/2')
       .send({ artist: 'Matthew Applegate' });
+
+    expect(resp.body).toEqual(expected);
+  });
+
+  it('deletes an existing entry in the artists table', async () => {
+    const expected = Artist.findById(2);
+    const resp = await request(app).delete('/api/v1/artists/2');
 
     expect(resp.body).toEqual(expected);
   });
